ef04d60c921d692dd587afa1e68c593290344cf1
IGeraGera <gerasimoi@ece.auth.gr>
2021-11-21 05:19:53 +0200

    config.def.h : kitty as default terminal

--- config.def.h
+++ config.def.h
@@ -84,14 +84,14 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 /* Terminal definition */
-#define TERM "alacritty"
+#define TERM "kitty"
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { TERM, NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, "-d", "120","34", NULL };
+static const char *scratchpadcmd[] = { "kitty", "-t", scratchpadname, "-d", "120","34", NULL };
 static const char *tuifilemancmd[] = { TERM, "-e", "bash","-c","$HOME/bin/readWal; ranger","NULL"};
 static const char *guifilemancmd[] = { "pcmanfm",NULL};
 static const char *browsercmd[] = { "firefox", NULL  };
